711d323eef9eb2a44679acae5fc780d9b7baa07d
scottquashen <scottquashen.sq@gmail.com>
2023-07-29 16:51:04 -0700

    added multi theme support.
    next, add a button to change the theme.
    
    then, add animation to run through each theme as you change through them

--- brain_game/ViewController.swift
+++ brain_game/ViewController.swift
@@ -10,6 +10,12 @@
 // adding app icons
 // ready for publishing 7.21.23
 
+// ading multi theme support
+
+// adding animation on screen on load
+// adding animation on screen when new color pallet selected
+// using the color pallets for the colors on screen instead of using constants to define each 'button's' color
+
 import UIKit
 import AVFoundation
 
@@ -17,6 +23,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        intitColorPallets()
         playSoundMP3(sound: "lose")
   
         // Do any additional setup after loading the view.
@@ -25,7 +32,7 @@ class ViewController: UIViewController {
 //        bottomLeftView.backgroundColor = UIColor.black
 //        bottomRightView.backgroundColor = UIColor.black
         setFourViewsToWhite()
-        gamePlayMessagesBox.backgroundColor = UIColor.green
+        gamePlayMessagesBox.backgroundColor = UIColor.clear
         winLabel.text = ""
         
     }
@@ -180,10 +187,150 @@ class ViewController: UIViewController {
     }
 
     
-    let topLeftColorKey = UIColor.red
-    let topRightColorKey = UIColor.blue
-    let bottomLeftColorKey = UIColor.green
-    let bottomRightColorKey = UIColor.yellow
+    var topLeftColorKey = #colorLiteral(red: 0.9921568627, green: 0, blue: 0.3764705882, alpha: 1)
+    var topRightColorKey = #colorLiteral(red: 0.06666666667, green: 0.4745098039, blue: 1, alpha: 1)
+    var bottomLeftColorKey = #colorLiteral(red: 0.1176470588, green: 0.8745098039, blue: 0.6352941176, alpha: 1)
+    var bottomRightColorKey = #colorLiteral(red: 0.9647058824, green: 0.9803921569, blue: 0.4392156863, alpha: 1)
+    
+
+    var colorPallets: [[_ColorLiteralType]] = [
+//        colorGroup0, colorGroup1, colorGroup2, colorGroup3, colorGroup4, colorGroup5, colorGroup6, colorGroup7. colorGroup8, colorGroup9, colorGroup10, colorGroup11, colorGroup12, colorGroup13, colorGroup14, colorGroup15, colorGroup16, colorGroup17, colorGroup18, colorGroup19, colorGroup20, colorGroup21,
+    ]
+    
+    var indexForIteratingTheColorPallets = 0
+    
+    func changeTheme(){
+        if(indexForIteratingTheColorPallets < colorPallets.count - 1){
+            indexForIteratingTheColorPallets = indexForIteratingTheColorPallets + 1
+        } else {
+            indexForIteratingTheColorPallets = 0
+        }
+     
+        topLeftColorKey = colorPallets[indexForIteratingTheColorPallets][0]
+        topRightColorKey = colorPallets[indexForIteratingTheColorPallets][1]
+        bottomLeftColorKey = colorPallets[indexForIteratingTheColorPallets][2]
+        bottomRightColorKey = colorPallets[indexForIteratingTheColorPallets][3]
+    }
+    
+    
+ 
+
+    func intitColorPallets(){
+        colorPallets.append(colorGroup0)
+        colorPallets.append(colorGroup1)
+        colorPallets.append(colorGroup2)
+        colorPallets.append(colorGroup3)
+        colorPallets.append(colorGroup4)
+        colorPallets.append(colorGroup5)
+        colorPallets.append(colorGroup6)
+        colorPallets.append(colorGroup7)
+        colorPallets.append(colorGroup8)
+        colorPallets.append(colorGroup9)
+        colorPallets.append(colorGroup10)
+        colorPallets.append(colorGroup11)
+        colorPallets.append(colorGroup12)
+        colorPallets.append(colorGroup13)
+        colorPallets.append(colorGroup14)
+        colorPallets.append(colorGroup15)
+        colorPallets.append(colorGroup16)
+        colorPallets.append(colorGroup17)
+        colorPallets.append(colorGroup18)
+        colorPallets.append(colorGroup19)
+        colorPallets.append(colorGroup20)
+        colorPallets.append(colorGroup21)
+    }
+
+    
+  let colorGroup0 = [
+    #colorLiteral(red: 0.9916704297, green: 0.000231181024, blue: 0.3763501942, alpha: 1),  #colorLiteral(red: 0.06666666667, green: 0.4745098039, blue: 1, alpha: 1), #colorLiteral(red: 0.1176470588, green: 0.8745098039, blue: 0.6352941176, alpha: 1), #colorLiteral(red: 0.9647058824, green: 0.9803921569, blue: 0.4392156863, alpha: 1),
+    ]
+    
+    let colorGroup1 = [
+        #colorLiteral(red: 0.05911300331, green: 0.1888224185, blue: 0.2514714003, alpha: 1),  #colorLiteral(red: 1, green: 0.3303389847, blue: 0.3934669495, alpha: 1),  #colorLiteral(red: 0.98600775, green: 0.7472785115, blue: 0.3501231074, alpha: 1),  #colorLiteral(red: 0.929158628, green: 0.8650338054, blue: 0.7671177983, alpha: 1),
+    ]
+    
+    let colorGroup2 = [
+        #colorLiteral(red: 0.8392156863, green: 0.07450980392, blue: 0.3333333333, alpha: 1),  #colorLiteral(red: 0.9764705882, green: 0.2901960784, blue: 0.1607843137, alpha: 1),  #colorLiteral(red: 0.9882352941, green: 0.8862745098, blue: 0.1647058824, alpha: 1),  #colorLiteral(red: 0.1882352941, green: 0.8901960784, blue: 0.8745098039, alpha: 1),
+    ]
+    
+    let colorGroup3 = [
+        #colorLiteral(red: 0.1490196078, green: 0, blue: 0.1058823529, alpha: 1),  #colorLiteral(red: 0.5058823529, green: 0, blue: 0.2039215686, alpha: 1),  #colorLiteral(red: 1, green: 0, blue: 0.3607843137, alpha: 1),  #colorLiteral(red: 1, green: 0.9647058824, blue: 0, alpha: 1),
+    ]
+    
+    let colorGroup4 = [
+        #colorLiteral(red: 0.09019607843, green: 0.09019607843, blue: 0.09019607843, alpha: 1),  #colorLiteral(red: 0.2666666667, green: 0.2666666667, blue: 0.2666666667, alpha: 1),  #colorLiteral(red: 0.8549019608, green: 0, blue: 0.2156862745, alpha: 1),  #colorLiteral(red: 0.9294117647, green: 0.9294117647, blue: 0.9294117647, alpha: 1),
+    ]
+    
+    let colorGroup5 = [
+        #colorLiteral(red: 0.8666666667, green: 0.8666666667, blue: 0.8666666667, alpha: 1),  #colorLiteral(red: 0.9764705882, green: 0.9529411765, blue: 0.9529411765, alpha: 1),  #colorLiteral(red: 0.968627451, green: 0.8509803922, blue: 0.8509803922, alpha: 1),  #colorLiteral(red: 0.9490196078, green: 0.3215686275, blue: 0.5294117647, alpha: 1),
+    ]
+    
+    let colorGroup6 = [
+        #colorLiteral(red: 0.9450980392, green: 0.1019607843, blue: 0.4823529412, alpha: 1),  #colorLiteral(red: 0.5960784314, green: 0.1294117647, blue: 0.462745098, alpha: 1),  #colorLiteral(red: 0.2431372549, green: 0, blue: 0.1215686275, alpha: 1),  #colorLiteral(red: 1, green: 0.8980392157, blue: 0.6784313725, alpha: 1),
+    ]
+    
+    let colorGroup7 = [
+        #colorLiteral(red: 0.05098039216, green: 0.07058823529, blue: 0.5098039216, alpha: 1),  #colorLiteral(red: 0.9333333333, green: 0.9294117647, blue: 0.9294117647, alpha: 1),  #colorLiteral(red: 0.9411764706, green: 0.8705882353, blue: 0.2117647059, alpha: 1),  #colorLiteral(red: 0.8431372549, green: 0.07450980392, blue: 0.07450980392, alpha: 1),
+    ]
+    
+    let colorGroup8 = [
+        #colorLiteral(red: 0.9254901961, green: 0.9725490196, blue: 0.9764705882, alpha: 1),  #colorLiteral(red: 0.02352941176, green: 0.5529411765, blue: 0.662745098, alpha: 1),  #colorLiteral(red: 0.4941176471, green: 0.09019607843, blue: 0.09019607843, alpha: 1),  #colorLiteral(red: 0.8980392157, green: 0.3450980392, blue: 0.02745098039, alpha: 1),
+    ]
+    
+    let colorGroup9 = [
+        #colorLiteral(red: 0.6039215686, green: 0.1254901961, blue: 0.5490196078, alpha: 1),  #colorLiteral(red: 0.8823529412, green: 0.07058823529, blue: 0.6, alpha: 1),  #colorLiteral(red: 1, green: 0.9176470588, blue: 0.9176470588, alpha: 1),  #colorLiteral(red: 0.9607843137, green: 0.7764705882, blue: 0.9254901961, alpha: 1),
+    ]
+    
+    let colorGroup10 = [
+        #colorLiteral(red: 0.6, green: 0.08235294118, blue: 0.3058823529, alpha: 1),  #colorLiteral(red: 1, green: 0.7882352941, blue: 0.2352941176, alpha: 1),  #colorLiteral(red: 1, green: 0.8666666667, blue: 0.8, alpha: 1),  #colorLiteral(red: 1, green: 0.7333333333, blue: 0.8, alpha: 1),
+    ]
+    
+    let colorGroup11 = [
+        #colorLiteral(red: 1, green: 0.7882352941, blue: 0.5882352941, alpha: 1),  #colorLiteral(red: 1, green: 0.5176470588, blue: 0.4549019608, alpha: 1),  #colorLiteral(red: 0.6235294118, green: 0.3725490196, blue: 0.5019607843, alpha: 1),  #colorLiteral(red: 0.3450980392, green: 0.2392156863, blue: 0.4470588235, alpha: 1),
+    ]
+    
+    let colorGroup12 = [
+        #colorLiteral(red: 0.1294117647, green: 0.1411764706, blue: 0.2392156863, alpha: 1),  #colorLiteral(red: 1, green: 0.4862745098, blue: 0.4862745098, alpha: 1),  #colorLiteral(red: 1, green: 0.8156862745, blue: 0.5098039216, alpha: 1),  #colorLiteral(red: 0.5333333333, green: 0.8823529412, blue: 0.9490196078, alpha: 1),
+    ]
+    
+    
+    let colorGroup13 = [
+        #colorLiteral(red: 1, green: 0.9647058824, blue: 0.8549019608, alpha: 1),  #colorLiteral(red: 0.5176470588, green: 0.9490196078, blue: 0.8392156863, alpha: 1),  #colorLiteral(red: 0.9882352941, green: 0.4196078431, blue: 0.2470588235, alpha: 1),  #colorLiteral(red: 0.1490196078, green: 0.1450980392, blue: 0.1450980392, alpha: 1),
+    ]
+    
+    let colorGroup14 = [
+        #colorLiteral(red: 0.2078431373, green: 0.8156862745, blue: 0.7294117647, alpha: 1),  #colorLiteral(red: 0.9725490196, green: 0.768627451, blue: 0.2274509804, alpha: 1),  #colorLiteral(red: 0.7882352941, green: 0.2392156863, blue: 0.1058823529, alpha: 1),  #colorLiteral(red: 0.01568627451, green: 0.1960784314, blue: 0.1803921569, alpha: 1),
+    ]
+    
+    let colorGroup15 = [
+        #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1),  #colorLiteral(red: 0.231372549, green: 0.03529411765, blue: 0.2666666667, alpha: 1),  #colorLiteral(red: 0.3725490196, green: 0.09411764706, blue: 0.3294117647, alpha: 1),  #colorLiteral(red: 0.1019607843, green: 0.7333333333, blue: 0.6117647059, alpha: 1),
+    ]
+    
+    let colorGroup16 = [
+        #colorLiteral(red: 0.8, green: 0.9411764706, blue: 0.7647058824, alpha: 1),  #colorLiteral(red: 0.737254902, green: 0.6392156863, blue: 0.7921568627, alpha: 1),  #colorLiteral(red: 0.4862745098, green: 0.2784313725, blue: 0.537254902, alpha: 1),  #colorLiteral(red: 0.2901960784, green: 0.05490196078, blue: 0.3607843137, alpha: 1),
+    ]
+    
+    let colorGroup17 = [
+        #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1),  #colorLiteral(red: 0.8, green: 0.9176470588, blue: 0.7333333333, alpha: 1),  #colorLiteral(red: 0.2470588235, green: 0.2470588235, blue: 0.2666666667, alpha: 1),  #colorLiteral(red: 0.9921568627, green: 0.7960784314, blue: 0.6196078431, alpha: 1),
+    ]
+    
+    let colorGroup18 = [
+        #colorLiteral(red: 0.9843137255, green: 0.9254901961, blue: 0.9254901961, alpha: 1),  #colorLiteral(red: 0.568627451, green: 0.8196078431, blue: 0.5450980392, alpha: 1),  #colorLiteral(red: 0.8823529412, green: 0.1137254902, blue: 0.4549019608, alpha: 1),  #colorLiteral(red: 0.2666666667, green: 0, blue: 0.2784313725, alpha: 1),
+    ]
+    
+    let colorGroup19 = [
+        #colorLiteral(red: 1, green: 0.6039215686, blue: 0.462745098, alpha: 1),  #colorLiteral(red: 1, green: 0.9176470588, blue: 0.8588235294, alpha: 1),  #colorLiteral(red: 0.4039215686, green: 0.6078431373, blue: 0.6078431373, alpha: 1),  #colorLiteral(red: 0.3882352941, green: 0.4509803922, blue: 0.4509803922, alpha: 1),
+    ]
+    
+    let colorGroup20 = [
+        #colorLiteral(red: 0.6352941176, green: 0.8235294118, blue: 1, alpha: 1),  #colorLiteral(red: 0.9960784314, green: 0.9764705882, blue: 0.937254902, alpha: 1),  #colorLiteral(red: 1, green: 0.5254901961, blue: 0.368627451, alpha: 1),  #colorLiteral(red: 0.9960784314, green: 0.8941176471, blue: 0.2509803922, alpha: 1),
+    ]
+    
+    let colorGroup21 = [
+        #colorLiteral(red: 1, green: 0, blue: 0.4588235294, alpha: 1),  #colorLiteral(red: 0.09019607843, green: 0.1529411765, blue: 0.4549019608, alpha: 1),  #colorLiteral(red: 0.4666666667, green: 0.8509803922, blue: 0.4392156863, alpha: 1),  #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1),
+    ]
+    
+    
     
     var justLeveledUp = false
     
@@ -202,7 +349,7 @@ class ViewController: UIViewController {
                     self.gamePlayMessagesLabel.text = ""
                     self.setFourViewsToWhite()
 
-                    self.winLabel.text = "😎"
+                    self.winLabel.text = "🎉"
                     self.gamePlayMessagesBox.backgroundColor = UIColor.white
                     DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                         self.gamePlayMessagesBox.backgroundColor = UIColor.white
